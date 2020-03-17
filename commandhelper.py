c888e9dfa0615297ea80d315cf34e73a55afd904
luffy <uputpal16@gmail.com>
2020-03-18 03:00:18 +0530

    buttons

--- commandhelper.py
+++ commandhelper.py
@@ -3,19 +3,21 @@ import iconactions as ica
 from telethon.sync import TelegramClient
 import asyncio
 from telegram.ext.dispatcher import run_async
+from telegram import  InlineKeyboardButton, InlineKeyboardMarkup
 import threading
+import json
 
 
-def commands(bot, update):
+def commands(update, context):
     user = update.message.from_user.username
-    bot.send_message(
+    context.bot.send_message(
         chat_id=update.message.chat_id,
         text="Initiating commands /tip has a specfic format,\n use it like so:"
         + "\n \n Parameters: \n <user> = target user to tip \n <token type> = icx or irc2 \n <amount> = amount of token to utilise  \n \n Tipping format: \n /tip <user> <toke type> <amount> ",
     )
 
 # @run_async
-def airdrop(bot, update):
+def airdrop(update, context):
     user = update.message.from_user.username
     message = update.message.text
     args = message.split(" ")
@@ -29,7 +31,7 @@ def airdrop(bot, update):
     print("printing at commandhandler"+ update.message.chat.type)
     if update.message.chat.type == "private":
         print("in private")
-        bot.send_message(
+        context.bot.send_message(
             chat_id=update.message.chat_id,
             text= 'Airdrop only works in a group'
         )
@@ -38,192 +40,190 @@ def airdrop(bot, update):
     try:
         amount = int(float(args[1]) * (10 ** 18))
     except:
-        bot.send_message(
+        context.bot.send_message(
             chat_id=update.message.chat_id, text="Amount should be a sane number!!!!!",
         )
+        return
     token = args[2].lower()
-    bot.send_message(
+    context.bot.send_message(
             chat_id=update.message.chat_id, text="Airdrop in process!!!!!",
         )
-    ica.airdrop(bot, update, amount, sender, token, user, loop)
-    # print(amount)
-    # print(sender)
-    # print(token)
-    # print(user)
-    # tx_hash_list, error_list, message =
-    # task = loop.create_task(ica.airdrop(amount, sender, token, user))
-    # remaining_work_not_depends_on_foo()
-    # ica.airdrop(amount, sender, token, user)
-    # t = threading.Thread(target=ica.airdrop, args = (bot, update, amount,sender, token, user,))
-    # t.daemon = True
-    # t.start()
-    # bot.send_message(
-    #     chat_id=update.message.chat_id,
-    #     text="Airdrop Successful for: "
-    #     # + str(tx_hash_list)
-    #     + "\nFailed Tx: "
-    #     # + str(error_list)
-    #     + "\n"
-    #     # + message,
-    # )
-    # except:
-    #     bot.send_message(
-    #         chat_id=update.message.chat_id, text="Encountered an  error!!!!!",
-    #     )
-    # loop.run_until_complete(task)
+    ica.airdrop(context.bot, update, amount, sender, token, user, loop)
+    
 
 # get private keys
-def get_keys(bot, update):
+def get_keys(update, context):
     print(update.message.chat)
     group = update.message.chat.type != "private"
     if group:
-        bot.send_message(
+        context.bot.send_message(
             chat_id=update.message.chat_id,
             text="Please ask for private keys from a private chat",
         )
+        
     else:
         private_key, address, message = ica.get_keys(
             update.message.from_user.username
         )
         if private_key is None:
-            bot.send_message(chat_id=update.message.chat_id, text=message)
+            context.bot.send_message(chat_id=update.message.chat_id, text=message)
         else:
-            bot.send_message(
+            context.bot.send_message(
                 chat_id=update.message.chat_id,
                 text="Public Address: \n" + address + "\nPrivate Key: \n" + private_key,
             )
 
 
-def help(bot, update):
+def help(update, context):
     print(update)
-    bot.send_message(
+    context.bot.send_message(
         chat_id=update.message.chat_id,
         text="The following commands are at your disposal: /start , /hi , /commands , /tip, /balance",
     )
 
 
 # TODO: deposit icon
-def deposit(bot, update):
+def deposit(update, context):
     group = update.message.chat.type == "group"
     if group:
-        bot.send_message(
+        context.bot.send_message(
             chat_id=update.message.chat_id,
             text="Please ask for deposit address from private chat",
         )
+        return
     else:
         private_key, address, message = ica.get_keys(update.message.from_user.username)
         if private_key is None:
-            bot.send_message(chat_id=update.message.chat_id, text=message)
+            context.bot.send_message(chat_id=update.message.chat_id, text=message)
         else:
-            bot.send_message(
+            context.bot.send_message(
                 chat_id=update.message.chat_id,
                 text="Your Deposit Address is: \n" + address,
             )
 
 
-def tip(bot, update):
+def tip(update, context):
     user = update.message.from_user.username
     message = update.message.text
     args = message.split(" ")
     sender = md.find_one({"telegramUserId": user})
     try:
-        if update.message.reply_to_message["from"]["is_bot"]:
-            bot.send_message(
+        print("=======tip=======")
+        print(update.message.reply_to_message.from_user.is_bot)
+        if update.message.reply_to_message.from_user.is_bot:
+            context.bot.send_message(
                 chat_id=update.message.chat_id,
                 text="You can't tip a bot!!",
             )
-        receiver_id = update.message.reply_to_message["from"]["username"]
+            return
+        receiver_id = update.message.reply_to_message.from_user.username
+        token = args[2]
+        amount_input = args[1]
     except:
         receiver_id = args[1][1:]
+        token = args[2]
+        amount_input = args[3]
     receiver = md.find_one({"telegramUserId": receiver_id})
     amount = 0
     print("=======tip=======")
     print(receiver_id)
     if user is None:
-        bot.send_message(
+        context.bot.send_message(
             chat_id=update.message.chat_id,
             text="Please set a telegram username in your profile settings!",
         )
+        return
     elif sender is None:
-        bot.send_message(
+        context.bot.send_message(
             chat_id=update.message.chat_id,
             text="Please create an account to start tipping people",
         )
+        return
     elif receiver is None:
-        bot.send_message(
+        context.bot.send_message(
             chat_id=update.message.chat_id,
             text="Please ask @{0} to join our channel and create account".format(
-                args[1][1:]
+                receiver_id
             ),
         )
+        return
     try:
-        amount = int(float(args[3]) * (10 ** 18))
+        amount = int(float(amount_input) * (10 ** 18))
         print(amount)
     except:
-        bot.send_message(
+        context.bot.send_message(
             chat_id=update.message.chat_id, text="Amount should be a sane number!!!!!",
         )
+        return
     else:
-        if args[2].lower() == "icx":
+        if token.lower() == "icx":
             tx_hash, message = ica.tip_icx(amount, sender, receiver["address"])
             if tx_hash is None:
-                bot.send_message(
+                context.bot.send_message(
                     chat_id=update.message.chat_id, text=message,
                 )
+                return
             else:
-                bot.send_message(
+                context.bot.send_message(
                     chat_id=update.message.chat_id, text=message,
                 )
-        elif args[2].lower() == "irc2":
+                return
+        elif token.lower() == "irc2":
             tx_hash, message = ica.tip_irc(amount, sender, receiver["address"])
             if tx_hash is None:
-                bot.send_message(
+                context.bot.send_message(
                     chat_id=update.message.chat_id, text=message,
                 )
+                return
             else:
-                bot.send_message(
+                context.bot.send_message(
                     chat_id=update.message.chat_id, text=message,
                 )
+                return
         else:
-            bot.send_message(
+            context.bot.send_message(
                 chat_id=update.message.chat_id,
                 text="Please follow The format to send\nICX: /tip @username ICX value\nIRC2: /tip @username IRC2 value",
             )
+            return
 
 
-def balance(bot, update):
+def balance(update, context):
     user = update.message.from_user.username
     if user is None:
-        bot.send_message(
+        context.bot.send_message(
             chat_id=update.message.chat_id,
             text="Please set a telegram username in your profile settings!",
         )
+        return
     else:
         sender, balance_icx, balance_irc = ica.get_balance_user(user)
         if not sender:
-            bot.send_message(
+            context.bot.send_message(
                 chat_id=update.message.chat_id,
                 text="Please create an account to start tipping people".format(
                     args[1][1:]
                 ),
             )
+            return
         else:
-            bot.send_message(
+            context.bot.send_message(
                 chat_id=update.message.chat_id,
                 text="Your Wallet Balance is\nICX: "
                 + str(balance_icx)
                 + "\nIRC2: "
                 + str(balance_irc),
             )
+            return
 
 
 # TODO
-def price(bot, update):
+def price(update, context):
     user = update.message.from_user.username
 
 
 
-def withdraw(bot, update):
+def withdraw(update, context):
     user = update.message.from_user.username
     message = update.message.text
     args = message.split(" ")
@@ -231,12 +231,12 @@ def withdraw(bot, update):
     receiver = args[3]
     amount = 0
     if user is None:
-        bot.send_message(
+        context.bot.send_message(
             chat_id=update.message.chat_id,
             text="Please set a telegram username in your profile settings!!",
         )
     elif sender is None:
-        bot.send_message(
+        context.bot.send_message(
             chat_id=update.message.chat_id,
             text="Please create an account to start tipping people",
         )
@@ -244,61 +244,82 @@ def withdraw(bot, update):
         amount = int(float(args[1]) * (10 ** 18))
         print(amount)
     except:
-        bot.send_message(
+        context.bot.send_message(
             chat_id=update.message.chat_id, text="Amount should be a sane number!!!!!",
         )
     else:
         if args[2].lower() == "icx":
             tx_hash, message = ica.tip_icx(amount, sender, receiver)
             if tx_hash is None:
-                bot.send_message(
+                context.bot.send_message(
                     chat_id=update.message.chat_id, text=message,
                 )
             else:
-                bot.send_message(
+                context.bot.send_message(
                     chat_id=update.message.chat_id, text=message,
                 )
         elif args[2].lower() == "irc2":
             tx_hash, message = ica.tip_irc(amount, sender, receiver)
             if tx_hash is None:
-                bot.send_message(
+                context.bot.send_message(
                     chat_id=update.message.chat_id, text=message,
                 )
             else:
-                bot.send_message(
+                context.bot.send_message(
                     chat_id=update.message.chat_id, text=message,
                 )
         else:
-            bot.send_message(
+            context.bot.send_message(
                 chat_id=update.message.chat_id,
                 text="Please follow The format to withdraw\nICX: /withdraw <value> ICX <address>\nIRC2: /withdraw <value> ICX <address>",
             )
 
 
 # Dummy
-def hi(bot, update):
+def hi(update, context):
     user = update.message.from_user.username
-    bot.send_message(
+    context.bot.send_message(
         chat_id=update.message.chat_id,
         text="Hello @{0}, how are you doing today?".format(user),
     )
 
 
+
 # start command to create a wallet and map to user name
-def start(bot, update):
-    user = update.message.from_user.username
-    tel_user = md.find_one({"telegramUserId": user})
-    if tel_user is None:
-        pub_k = ica.create_wallet(user)
-        # print(response)
-        bot.send_message(
-            chat_id=update.message.chat_id,
-            text="Hello @{0}, how are you doing today?".format(user)
-            + "\nWe have set up you wallet, Your address is "
-            + pub_k,
-        )
-    else:
-        bot.send_message(
-            chat_id=update.message.chat_id,
-            text="Hello @{0}, how are you doing today?".format(user),
-        )
+def start(update, context):
+    
+    
+    keyboard = [[InlineKeyboardButton("Help", callback_data='/help')],
+                 [InlineKeyboardButton("Wallet", callback_data='/hi')],
+                [InlineKeyboardButton("Apps", callback_data='3')]]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    # update.callback_query.edit_message_text(text="Test", reply_markup=reply_markup)
+
+    update.message.reply_text('Welcome to Pluttest, a telegram tipper Bot for Icon Blockchain:',
+                               reply_markup=reply_markup)
+   
+   
+
+def button(update, context):
+    query = update.callback_query
+    print(query.data)
+    # query.edit_message_text(text="")
+    help(update, context)
+   
+# user = update.message.from_user.username
+# tel_user = md.find_one({"telegramUserId": user})
+# if tel_user is None:
+#     pub_k = ica.create_wallet(user)
+#     # print(response)
+#     context.bot.send_message(
+#         chat_id=update.message.chat_id,
+#         text="Hello @{0}, how are you doing today?".format(user)
+#         + "\nWe have set up you wallet, Your address is "
+#         + pub_k,
+#     )
+# else:
+#     context.bot.send_message(
+#         chat_id=update.message.chat_id,
+#         text="Hello @{0}, how are you doing today?".format(user),
+#     )
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from telegram.ext import CommandHandler
+from telegram.ext import CommandHandler, CallbackQueryHandler
 from telegram.ext.dispatcher import run_async
 from telegram.ext import Updater
 import math
@@ -8,44 +8,57 @@ import logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
+logger = logging.getLogger(__name__)
 
-updater = Updater(token=cfg.config["bot_token"])
-dispatcher = updater.dispatcher
 
-import commandhelper as ch
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+    
+def main():    
+    updater = Updater(token=cfg.config["bot_token"], use_context=True)
+    dispatcher = updater.dispatcher
 
-commands_handler = CommandHandler("commands", ch.commands)
-dispatcher.add_handler(commands_handler)
+    import commandhelper as ch
 
-hi_handler = CommandHandler("hi", ch.hi)
-dispatcher.add_handler(hi_handler)
+    commands_handler = CommandHandler("commands", ch.commands)
+    dispatcher.add_handler(commands_handler)
 
-start_handler = CommandHandler("start", ch.start)
-dispatcher.add_handler(start_handler)
+    hi_handler = CommandHandler("hi", ch.hi)
+    dispatcher.add_handler(hi_handler)
 
-deposit_handler = CommandHandler("deposit", ch.deposit)
-dispatcher.add_handler(deposit_handler)
+    start_handler = CommandHandler("start", ch.start)
+    dispatcher.add_handler(start_handler)
 
-price_handler = CommandHandler("price", ch.price)
-dispatcher.add_handler(price_handler)
+    deposit_handler = CommandHandler("deposit", ch.deposit)
+    dispatcher.add_handler(deposit_handler)
 
-tip_handler = CommandHandler("tip", ch.tip)
-dispatcher.add_handler(tip_handler)
+    price_handler = CommandHandler("price", ch.price)
+    dispatcher.add_handler(price_handler)
 
-balance_handler = CommandHandler("balance", ch.balance)
-dispatcher.add_handler(balance_handler)
+    tip_handler = CommandHandler("tip", ch.tip)
+    dispatcher.add_handler(tip_handler)
 
-help_handler = CommandHandler("help", ch.help)
-dispatcher.add_handler(help_handler)
+    balance_handler = CommandHandler("balance", ch.balance)
+    dispatcher.add_handler(balance_handler)
 
-airdrop_handler = CommandHandler("airdrop", ch.airdrop)
-dispatcher.add_handler(airdrop_handler)
+    help_handler = CommandHandler("help", ch.help)
+    dispatcher.add_handler(help_handler)
 
-export_handler = CommandHandler("export", ch.get_keys)
-dispatcher.add_handler(export_handler)
+    airdrop_handler = CommandHandler("airdrop", ch.airdrop)
+    dispatcher.add_handler(airdrop_handler)
 
-withdraw_handler = CommandHandler("withdraw", ch.withdraw)
-dispatcher.add_handler(withdraw_handler)
+    export_handler = CommandHandler("export", ch.get_keys)
+    dispatcher.add_handler(export_handler)
 
-updater.start_polling()
-# updater.idle()
+    withdraw_handler = CommandHandler("withdraw", ch.withdraw)
+    dispatcher.add_handler(withdraw_handler)
+
+    updater.dispatcher.add_error_handler(error)
+    updater.dispatcher.add_handler(CallbackQueryHandler(ch.button))
+    
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
